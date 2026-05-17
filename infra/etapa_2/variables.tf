8774f79b6a656f5ff4227118f112e0634a3476d2
kaiser002 <be.valdebenitoc@duocuc.cl>
2026-05-17 00:20:39 -0400

    feat: desplegar en arquitectura distribuida de 4 ec2 v3

--- infra/etapa_2/variables.tf
+++ infra/etapa_2/variables.tf
@@ -10,7 +10,7 @@ variable "instance_type" {
 
 variable "key_name" {
   description = "Nombre de la llave SSH (vkey) ya creada en AWS"
-  default     = "proyecto_key"
+  default     = "labsuser"
 }
 
 variable "cluster_name" {
